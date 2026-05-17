b0a6c95e34dfcc9788ae91023bcc3064041df49b
tobiaslenz1988 <tobias.lenz88@live.de>
2026-05-18 01:48:20 +0200

    Delete display/logos directory

--- display/logos/audi_alt_2.h
+++
@@ -1,9 +0,0 @@
-
-#define AUDI_LOGO_ALT_2_WIDTH 240
-#define AUDI_LOGO_ALT_2_HEIGHT 31
-/*
-created by tool https://www.online-utility.org/image/convert/to/XBM
-due to copyright the logos are deleted
-*/
-
-static uint8_t audi_alt_2[930];
--- display/logos/dodge_logo.h
+++
@@ -1,9 +0,0 @@
-#define DODGE_LOGOWIDTH 240
-#define DODGE_LOGOHEIGHT 240
-
-
-/*
-created by tool https://www.online-utility.org/image/convert/to/XBM
-due to copyright the logos are deleted
-*/
-static uint8_t dodge_logo[7200];
