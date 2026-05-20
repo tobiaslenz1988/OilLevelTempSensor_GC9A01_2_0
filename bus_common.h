fe863ab55418bbf0137c2cd6ada0bf4acac79ea5
tobiaslenz1988 <tobias.lenz88@live.de>
2026-05-20 22:42:23 +0200

    Add files via upload

--- bus_common.h
+++ bus_common.h
@@ -1,9 +1,11 @@
 #ifndef BUS_COMMON_H
 #define BUS_COMMON_H
+#include <cstdint>
+#include <BluetoothSerial.h>
 
 void BUS_output(uint8_t data);
 
-//void initBtConnection(String name);
+void initBtConnection(String name);
 
 
 #endif /* BUS_COMMON_H */
